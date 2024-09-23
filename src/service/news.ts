e6e3f22a6e4172a66e32b38e1ea152b11b5d8998
enjiecen <enjiecen@enjiecendeMacBook-Pro.local>
2024-09-23 17:17:47 +0800

    Deploy

--- src/service/news.ts
+++
@@ -1,6 +0,0 @@
-import http from  './http'
-import { newsList } from './mockData';
-
-export const getNewsList = () => {
-  return newsList;
-}
--- types/declare.d.ts
+++
@@ -1,4 +0,0 @@
-// // 引入本地文件报类型错误
-// declare module "*";
-// declare module "*.tsx";
-// declare module "*.mjs";
